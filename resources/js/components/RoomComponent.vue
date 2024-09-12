014735bcace812d5e157f0fb7f0795344a13e7fe
Fontalvojs <mejia_andres@hotmail.es>
2024-09-11 19:44:31 -0500

    Pequeño cambio en la funcion que envia el formulario del room

--- resources/js/components/RoomComponent.vue
+++ resources/js/components/RoomComponent.vue
@@ -1,57 +1,34 @@
 <template>
-    <div
-        class="container d-flex justify-content-center align-items-center mt-5"
-    >
+    <div class="container d-flex justify-content-center align-items-center mt-5">
         <div class="login-card">
             <div class="card-header">
                 <h3 style="font-weight: bold">
                     Log in to Room 911
-                    <span
-                        :class="{
-                            'text-unlocked shake-effect': form.success,
-                            'text-dark': !form.success,
-                        }"
-                        style="float: right"
-                        ><i
-                            :class="{
-                                'fas fa-unlock': form.success,
-                                'fas fa-lock': !form.success,
-                            }"
-                        ></i
-                    ></span>
+                    <span :class="{
+                        'text-unlocked shake-effect': form.success,
+                        'text-dark': !form.success,
+                    }" style="float: right"><i :class="{
+                        'fas fa-unlock': form.success,
+                        'fas fa-lock': !form.success,
+                    }"></i></span>
                 </h3>
             </div>
 
             <div class="card-body">
                 <form @submit.prevent="submitForm">
                     <div class="form-group mb-3">
-                        <label for="employee_id" class="form-label"
-                            >Your ID access</label
-                        >
-                        <input
-                            id="employee_id"
-                            v-model="form.employee_id"
-                            type="password"
-                            class="form-control"
-                            autofocus
-                            required
-                        />
+                        <label for="employee_id" class="form-label">Your ID access</label>
+                        <input id="employee_id" v-model="form.employee_id" type="password" class="form-control"
+                            autofocus required />
                         <div v-if="errors.employee_id" class="text-danger">
-                            <span
-                                v-for="(error, index) in errors.employee_id"
-                                :key="index"
-                            >
-                                {{ error }}</span
-                            >
+                            <span v-for="(error, index) in errors.employee_id" :key="index">
+                                {{ error }}</span>
                         </div>
                     </div>
 
                     <div class="form-group text-center">
-                        <button
-                            type="submit"
-                            class="btn btn-dark btn-block w-100 text-uppercase"
-                            style="font-weight: bold"
-                        >
+                        <button type="submit" class="btn btn-dark btn-block w-100 text-uppercase"
+                            style="font-weight: bold">
                             Verify
                         </button>
                     </div>
@@ -61,11 +38,8 @@
     </div>
 </template>
 <script>
-import "../../css/app.css";
-import { useToast } from "vue-toastification";
-import "vue-toastification/dist/index.css";
-const toast = useToast();
-import axios_instance from "../utils/axios";
+import { useThrottle } from "../utils/useThrottle";
+import { submitForm } from "../utils/room_methods";
 export default {
     name: "RoomComponent",
     data() {
@@ -81,47 +55,7 @@ export default {
         id: String,
     },
     methods: {
-        async submitForm() {
-            toast.info("Logging in to Room 911...", {
-                timeout: 3000,
-                position: "top-right",
-            });
-            this.errors = {}; // Reset errors
-            if (!this.form.employee_id) {
-                this.errors.employee_id = ["This field is required"];
-                return;
-            }
-
-            try {
-                const response = await axios_instance.post(
-                    "/login-room",
-                    this.form
-                );
-                if (response.status === 200) {
-                    this.form.success = true;
-                    toast.success("You'll be redirected", {
-                        timeout: 3000,
-                        position: "top-right",
-                    });
-                    await new Promise((resolve) =>
-                        setTimeout(() => {
-                            window.location.href = "https://www.lifefile.net/";
-                            resolve();
-                        }, 1000)
-                    );
-                }
-            } catch (error) {
-                if (error.response && error.response.data.errors) {
-                    this.errors = error.response.data.errors;
-                }
-                if (error.response.status === 403) {
-                    toast.error(error.response.data.error, {
-                        timeout: 3000,
-                        position: "top-right",
-                    });
-                }
-            }
-        },
+        submitForm: useThrottle(submitForm, 1000),
     },
 };
 </script>
---
+++ resources/js/utils/room_methods.js
@@ -0,0 +1,46 @@
+import "../../css/app.css";
+import { useToast } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+const toast = useToast();
+import axios_instance from "../utils/axios";
+export const submitForm = async function() {
+    toast.info("Logging in to Room 911...", {
+        timeout: 3000,
+        position: "top-right",
+    });
+    this.errors = {}; // Reset errors
+    if (!this.form.employee_id) {
+        this.errors.employee_id = ["This field is required"];
+        return;
+    }
+
+    try {
+        const response = await axios_instance.post(
+            "/login-room",
+            this.form
+        );
+        if (response.status === 200) {
+            this.form.success = true;
+            toast.success("You'll be redirected", {
+                timeout: 3000,
+                position: "top-right",
+            });
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    window.location.href = "https://www.lifefile.net/";
+                    resolve();
+                }, 1000)
+            );
+        }
+    } catch (error) {
+        if (error.response && error.response.data.errors) {
+            this.errors = error.response.data.errors;
+        }
+        if (error.response.status === 403) {
+            toast.error(error.response.data.error, {
+                timeout: 3000,
+                position: "top-right",
+            });
+        }
+    }
+}
